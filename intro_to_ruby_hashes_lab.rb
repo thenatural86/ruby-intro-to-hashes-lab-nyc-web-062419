3b3dd9d37d4aea00be341b5f574b450197e1903b
The Natural <philip.smalls86@gmail.com>
2019-06-01 17:27:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,7 @@ def monopoly_with_fourth_tier
  monopoly = {
     railroads: {
       pieces: 4, 
-      :names =>{
+      names: {
         :reading_railroad => {
           "mortgage_value" => "$100"
           }, 
